5f966d40c778c3e20dfadd1f977167f23b6ec79a
teilabs <tyshchenko.ei@yandex.ru>
2026-04-24 20:48:10 +0300

    Implement methods for generating base64 strings that represent Ed25519KeyPair and generating Ed25519KeyPair from base64 strings

--- core/src/main/java/io/github/teilabs/meshnet/core/crypto/Ed25519KeyPair.java
+++ core/src/main/java/io/github/teilabs/meshnet/core/crypto/Ed25519KeyPair.java
@@ -2,6 +2,7 @@ package io.github.teilabs.meshnet.core.crypto;
 
 import java.nio.ByteBuffer;
 import java.security.SecureRandom;
+import java.util.Base64;
 import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
 import org.bouncycastle.crypto.generators.Ed25519KeyPairGenerator;
 import org.bouncycastle.crypto.params.Ed25519KeyGenerationParameters;
@@ -17,7 +18,7 @@ import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
  */
 public record Ed25519KeyPair(byte[] privateKey, byte[] publicKey, long routingId) {
     public Ed25519KeyPair {
-        if (privateKey.length != 32 || publicKey.length != 32) 
+        if (privateKey.length != 32 || publicKey.length != 32)
             throw new IllegalArgumentException("Keys must be 32 bytes");
     }
 
@@ -45,5 +46,39 @@ public record Ed25519KeyPair(byte[] privateKey, byte[] publicKey, long routingId
      */
     public static long generateRoutingId(byte[] publicKey) {
         return ByteBuffer.wrap(publicKey, 0, 8).getLong();
+    }
+
+    /**
+     * Generates base64 encoded public key from key pair
+     * 
+     * @param keyPair key pair to generate encoded public key from
+     * @return base64 encoded public key
+     */
+    public static String toBase64Public(Ed25519KeyPair keyPair) {
+        return Base64.getEncoder().encodeToString(keyPair.publicKey);
+    }
+
+    /**
+     * Generates base64 encoded private key from key pair
+     * 
+     * @param keyPair key pair to generate encoded private key from
+     * @return base64 encoded private key
+     */
+    public static String toBase64Private(Ed25519KeyPair keyPair) {
+        return Base64.getEncoder().encodeToString(keyPair.privateKey);
+    }
+
+    /**
+     * Generates key pair from base64 encoded public and private keys
+     * 
+     * @param publicBase64  base64 encoded public key
+     * @param privateBase64 base64 encoded private key
+     * @return generated key pair
+     */
+    public static Ed25519KeyPair fromBase64(String publicBase64, String privateBase64) {
+        byte[] publicKey = Base64.getDecoder().decode(publicBase64);
+        byte[] privateKey = Base64.getDecoder().decode(privateBase64);
+        long routingId = generateRoutingId(publicKey);
+        return new Ed25519KeyPair(privateKey, publicKey, routingId);
     }
 }
